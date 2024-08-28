8f7b042c83ba4adeb95a16fce4f96bf0e562d27a
Manny-Codes18 <manuelmochiah@gmail.com>
2024-08-28 15:25:14 +0000

    feat: storage

---
+++ Arrays.sol
@@ -0,0 +1,48 @@
+
+
+pragma  solidity;
+>=0.7.0,<0.9.0;
+
+contract Arrays {
+    /*uint256[] arr;
+    
+    function add(|uint256 i) public{
+        arr.push(i)
+
+    }
+    
+    function getLength() public view returns(unit256){
+        return arr.length;
+    }
+
+    function getnumber() public view returns (unit256[] memory)
+    return arr;
+    
+    function 
+    
+    
+    
+    }*/
+     contract Arrays {
+     string name;
+     struct item;
+     string name;
+     bool verify;
+
+     item[] public students;
+     function  add {string memory _name, uint256}
+     
+     function create {string
+
+     }
+
+
+     function read () public view returns (item [] memory)\{
+        return students;
+     }
+     function update (uint256 id,string memory _name ) public{
+        uint i= find {id};
+        students[i].name = _name;
+     }
+
+     function remove() public 
---
+++ Map.sol
@@ -0,0 +1,26 @@
+
+pragma  solidity;
+>=0.7.0,<0.9.0;
+
+
+contract Map{
+
+    mapping(address => uint256) public amount;
+
+     function set (unit 256) public {
+     amount [msg.sender]=_amount;
+
+     }
+
+     function get () public returns (uint256){
+        return amount[msg.sender];
+
+     }
+      function update (unit256 _amount ) public
+
+
+
+
+
+}
+
